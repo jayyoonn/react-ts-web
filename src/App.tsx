d0bf0b82dc2c01e7fef73d2b9e0684ae14960307
jayyoonn <18819444437@163.com>
2023-07-20 14:16:51 +0800

    feat: sass、图片支持

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import './App.scss';
+import ReactLogo from './assets/React.png';
+
 export default function App() {
   return (
-    <div>
+    <div className="main">
       <h1>Welcome to React Web.</h1>
+      <img src={ReactLogo} alt="logo" />
     </div>
   );
 }
